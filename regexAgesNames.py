b81ef980e505760d682a45e8ef1a049fb74408be
Sarat Rajan <sarat.rajan4@t-mobile.com>
2020-08-17 15:43:34 -0700

    Regex names example

---
+++ regexAgesNames.py
@@ -0,0 +1,18 @@
+import re
+
+nameAge = '''
+Tom is 8 and Joe is 34
+Mary is 33
+'''
+
+ages = re.findall(r'\d{1,3}', nameAge)
+names = re.findall(r'[A-Z][a-z]*', nameAge)
+
+x = 0
+ageDict = {}
+
+for eachname in names:
+    ageDict[eachname] = ages[x]
+    x += 1
+
+print (ageDict)
