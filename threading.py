71b19452543ba1743e2557695f28ae07e3dbd25e
Sarat Rajan <sarat.rajan4@t-mobile.com>
2020-08-18 13:39:48 -0700

    Fixed all lint issues

--- threading.py
+++ threading.py
@@ -1,29 +1,41 @@
-#!/usr/bin/python
+""" Trying out threading mechanisms """
 
 import time
 import random
 
+
 def get_current_time():
+    """ Getting current time """
     return time.perf_counter()
 
 def lets_sleep(seconds):
+    """ Sleeping in thread """
     print(f"Sleeping {seconds} second(s) here...")
     time.sleep(seconds)
     print("Waking up now...")
 
+
 def calculate_elapsed_time(start_time, end_time):
+    """ Calculating time elapsed """
+    print("Getting elapsed time")
     return round(end_time - start_time, 2)
 
-
 def do_the_thing():
+    """ Action is happening here """
+    print("Getting current time")
     _start_time = get_current_time()
-    _seconds = random.randrange(10) 
+    print("Getting random sleep")
+    _seconds = random.randrange(10)
+    print("zzzz.....")
     lets_sleep(_seconds)
+    print("Getting current time")
     _end_time = get_current_time()
     _time_elapsed = calculate_elapsed_time(_start_time, _end_time)
     print(f'Finished in {_time_elapsed} second(s)...')
 
+
 def main():
+    """ I am the main dude """
     i = 1
     while i < 4:
         print(f'Attempt no: {i}')
